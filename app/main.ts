1d563f7df4cff34a18bb7b74929e9e155704502f
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 16:27:31 +0100

    refactor completed code

--- app/main.ts
+++ app/main.ts
@@ -4,119 +4,130 @@ import * as process from "process";
 import * as zlib from "zlib";
 import { Buffer } from "buffer";
 
-const server = net.createServer((socket: any) => {
-    socket.on("data", (data: any) => {
-        const request = data.toString();
-        console.log('Request:', request);
-        const [requestLine, ...headerLines] = request.split('\r\n');
-        const [method, path] = requestLine.split(' ');
+const handleGzipCompression = (data: string, callback: (err: Error | null, compressed?: Buffer) => void) => {
+    zlib.gzip(data, callback);
+};
 
-        if (!path) {
-            socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
-            socket.end();
-            return;
+const sendResponse = (socket: any, statusCode: string, headers: { [key: string]: string }, body: string | Buffer) => {
+    let response = `HTTP/1.1 ${statusCode}\r\n`;
+    for (const [key, value] of Object.entries(headers)) {
+        response += `${key}: ${value}\r\n`;
+    }
+    response += `\r\n`;
+    socket.write(response);
+    socket.write(body);
+    socket.end();
+};
+
+const handleRequest = (socket: any, request: string) => {
+    console.log('Request:', request);
+    const [requestLine, ...headerLines] = request.split('\r\n');
+    const [method, path] = requestLine.split(' ');
+
+    if (!path) {
+        sendResponse(socket, '400 Bad Request', {}, '');
+        return;
+    }
+
+    const headers: { [key: string]: string } = {};
+    for (const headerLine of headerLines) {
+        if (headerLine === '') break;
+        const [key, value] = headerLine.split(': ');
+        headers[key] = value;
+    }
+
+    const acceptEncoding = headers['Accept-Encoding'] || '';
+    const supportsGzip = acceptEncoding.includes('gzip');
+
+    if (method === 'GET') {
+        handleGetRequest(socket, path, headers, supportsGzip);
+    } else if (method === 'POST' && path.startsWith('/files/')) {
+        handlePostRequest(socket, path, headers, request);
+    } else {
+        sendResponse(socket, '405 Method Not Allowed', {}, '');
+    }
+};
+
+const handleGetRequest = (socket: any, path: string, headers: { [key: string]: string }, supportsGzip: boolean) => {
+    if (path === '/') {
+        sendResponse(socket, '200 OK', {}, '');
+    } else if (path.startsWith('/echo/')) {
+        const query = path.split("/")[2];
+        const responseHeaders: any = { 'Content-Type': 'text/plain' };
+
+        if (supportsGzip) {
+            handleGzipCompression(query, (err, compressed) => {
+                if (err) {
+                    console.error('Gzip Error:', err);
+                    sendResponse(socket, '500 Internal Server Error', {}, '');
+                } else {
+                    responseHeaders['Content-Encoding'] = 'gzip';
+                    responseHeaders['Content-Length'] = compressed!.length.toString();
+                    sendResponse(socket, '200 OK', responseHeaders, compressed!);
+                }
+            });
+        } else {
+            responseHeaders['Content-Length'] = query.length.toString();
+            sendResponse(socket, '200 OK', responseHeaders, query);
         }
-
-        const headers: { [key: string]: string } = {};
-        for (const headerLine of headerLines) {
-            if (headerLine === '') break;
-            const [key, value] = headerLine.split(': ');
-            headers[key] = value;
+    } else if (path === '/user-agent') {
+        const userAgent = headers['User-Agent'];
+        if (userAgent) {
+            const responseHeaders: any = { 'Content-Type': 'text/plain', 'Content-Length': userAgent.length.toString() };
+            sendResponse(socket, '200 OK', responseHeaders, userAgent);
+        } else {
+            sendResponse(socket, '400 Bad Request', {}, '');
         }
-
-        const acceptEncoding = headers['Accept-Encoding'] || '';
-        const supportsGzip = acceptEncoding.includes('gzip');
-
-        if (method === 'GET') {
-            if (path === '/') {
-                socket.write('HTTP/1.1 200 OK\r\n\r\n');
-            } else if (path.startsWith('/echo/')) {
-                const query = path.split("/")[2];
-                const responseHeaders = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n`;
-
+    } else if (path.startsWith('/files/')) {
+        const directory = process.argv[3];
+        const fileName = path.replace('/files/', '');
+        fs.readFile(`${directory}/${fileName}`, 'utf8', (err, fileData) => {
+            if (err) {
+                console.error('File Read Error:', err);
+                sendResponse(socket, '404 Not Found', {}, '');
+            } else {
+                const responseHeaders: any = { 'Content-Type': 'application/octet-stream' };
                 if (supportsGzip) {
-                    zlib.gzip(query, (err, compressed) => {
+                    handleGzipCompression(fileData, (err, compressed) => {
                         if (err) {
                             console.error('Gzip Error:', err);
-                            socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
-                            socket.end();
+                            sendResponse(socket, '500 Internal Server Error', {}, '');
                         } else {
-                            console.log('Compressed Data:', compressed);
-                            socket.write(`${responseHeaders}Content-Encoding: gzip\r\nContent-Length: ${compressed.length}\r\n\r\n`);
-                            socket.write(compressed);
-                            socket.end();
+                            responseHeaders['Content-Encoding'] = 'gzip';
+                            responseHeaders['Content-Length'] = compressed!.length.toString();
+                            sendResponse(socket, '200 OK', responseHeaders, compressed!);
                         }
                     });
                 } else {
-                    socket.write(`${responseHeaders}Content-Length: ${query.length}\r\n\r\n${query}`);
-                    socket.end();
+                    responseHeaders['Content-Length'] = Buffer.byteLength(fileData).toString();
+                    sendResponse(socket, '200 OK', responseHeaders, fileData);
                 }
-            } else if (path === '/user-agent') {
-                const userAgent = headers['User-Agent'];
-                if (userAgent) {
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-                } else {
-                    socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
-                }
-                socket.end();
-            } else if (path.startsWith('/files/')) {
-                const directory = process.argv[3];
-                console.log('Directory:', directory);
-                const fileName = path.replace('/files/', '');
-                console.log('File Name:', fileName);
-
-                fs.readFile(`${directory}/${fileName}`, 'utf8', (err: Error, fileData: string) => {
-                    if (err) {
-                        console.error('File Read Error:', err);
-                        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-                    } else {
-                        const responseHeaders = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\n`;
-
-                        if (supportsGzip) {
-                            zlib.gzip(fileData, (err, compressed) => {
-                                if (err) {
-                                    console.error('Gzip Error:', err);
-                                    socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
-                                } else {
-                                    socket.write(`${responseHeaders}Content-Encoding: gzip\r\nContent-Length: ${compressed.length}\r\n\r\n`);
-                                    socket.write(compressed);
-                                }
-                                socket.end();
-                            });
-                        } else {
-                            socket.write(`${responseHeaders}Content-Length: ${Buffer.byteLength(fileData)}\r\n\r\n${fileData}`);
-                            socket.end();
-                        }
-                    }
-                });
-                return;
-            } else {
-                socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-                socket.end();
             }
-        } else if (method === 'POST' && path.startsWith('/files/')) {
-            const directory = process.argv[3];
-            const fileName = path.replace('/files/', '');
-            console.log('POST Request for File:', fileName);
+        });
+    } else {
+        sendResponse(socket, '404 Not Found', {}, '');
+    }
+};
 
-            const contentLength = parseInt(headers['Content-Length'], 10);
-            const body = request.split('\r\n\r\n')[1].substring(0, contentLength);
-            console.log('Body:', body);
+const handlePostRequest = (socket: any, path: string, headers: { [key: string]: string }, request: string) => {
+    const directory = process.argv[3];
+    const fileName = path.replace('/files/', '');
+    const contentLength = parseInt(headers['Content-Length'], 10);
+    const body = request.split('\r\n\r\n')[1].substring(0, contentLength);
 
-            fs.writeFile(`${directory}/${fileName}`, body, 'utf8', (err: Error) => {
-                if (err) {
-                    console.error('File Write Error:', err);
-                    socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
-                } else {
-                    socket.write('HTTP/1.1 201 Created\r\n\r\n');
-                }
-                socket.end();
-            });
-            return;
+    fs.writeFile(`${directory}/${fileName}`, body, 'utf8', (err) => {
+        if (err) {
+            console.error('File Write Error:', err);
+            sendResponse(socket, '500 Internal Server Error', {}, '');
         } else {
-            socket.write('HTTP/1.1 405 Method Not Allowed\r\n\r\n');
-            socket.end();
+            sendResponse(socket, '201 Created', {}, '');
         }
+    });
+};
+
+const server = net.createServer((socket: any) => {
+    socket.on("data", (data: any) => {
+        handleRequest(socket, data.toString());
     });
 });
 

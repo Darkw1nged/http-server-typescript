41e689d303e96904639c4096f8bdc3f05d2d5658
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:47:17 +0100

    pass 8th stage

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import * as net from "net";
-import fs from "node:fs";
+import * as fs from "fs";
+import * as process from "process";
 
 const server = net.createServer((socket: any) => {
     socket.on("data", (data: any) => {
@@ -14,14 +15,17 @@ const server = net.createServer((socket: any) => {
         } else if (path === '/user-agent') {
             const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-        } else if (path === `/files/${query}`) {
-            fs.readFileSync(`./files/${query}`, 'utf8', (err: any, data: any) => {
+        } else if (path === `/files/`) {
+            const directory = process.argv[3];
+            const fileName = query;
+
+            fs.readFile(`${directory}${fileName}`, 'utf8', (err: Error, data: string) => {
                 if (err) {
                     socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-                } else {
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
                 }
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
             });
+
         }
 
 

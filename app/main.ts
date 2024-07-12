13dce8f982135ebd036e1734f7788f75afd97c20
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:20:56 +0100

    pass 4th stage

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,15 @@ const server = net.createServer((socket: any) => {
     socket.on("data", (data: any) => {
         const request = data.toString();
         const path = request.split(" ")[1];
+
+        if (path === '/echo/') {
+            const echo = request.split(" ")[2];
+            const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echo.length}\r\n\r\n${echo}`;
+            socket.write(res);
+            socket.end();
+            return;
+        }
+
         const res = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
         socket.write(res);
         socket.end();

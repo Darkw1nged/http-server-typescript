1b5516c61bda9190054e33b7b1c37e113fa65dae
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:54:36 +0100

    adding additional debug

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,9 @@ const server = net.createServer((socket: any) => {
         const path = request.split(" ")[1];
         const query = path.split("/")[2];
 
+        printf(path);
+        printf(query);
+
         if (path === '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
         } else if (path === `/echo/${query}`) {

bc6144a74292ceed9378fb21e0e102d4e577c6fe
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:17:55 +0100

    pass 3rd stage

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,11 @@
 import * as net from "net";
 
 const server = net.createServer((socket: any) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
-
-    socket.on("close", () => {
+    socket.on("data", (data: any) => {
+        const request = data.toString();
+        const path = request.split(" ")[1];
+        const res = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
+        socket.write(res);
         socket.end();
     });
 });

f36361c90ddd00e51bed1d9b9db1d101ed90e0f9
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:10:39 +0100

    pass 2nd stage

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,8 @@
 import * as net from "net";
 
-const server = net.createServer((socket) => {
+const server = net.createServer((socket: any) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+
     socket.on("close", () => {
         socket.end();
     });

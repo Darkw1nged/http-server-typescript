b614b8754e8a983855a622566afabc151effc2e3
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:33:35 +0100

    pass 8th stage

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as net from "net";
+import * as fs from "fs";
 
 const server = net.createServer((socket: any) => {
     socket.on("data", (data: any) => {
@@ -13,6 +14,14 @@ const server = net.createServer((socket: any) => {
         } else if (path === '/user-agent') {
             const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+        } else if (path === `/files/${query}`) {
+            fs.readFileSync(`./files/${query}`, 'utf8', (err: any, data: any) => {
+                if (err) {
+                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                } else {
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
+                }
+            });
         }
 
 

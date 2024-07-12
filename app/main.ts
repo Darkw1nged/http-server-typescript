7ea743a97ca181e062fbbb463055954aa44c41a1
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-07-12 14:48:11 +0100

    pass 8th stage

--- app/main.ts
+++ app/main.ts
@@ -15,7 +15,7 @@ const server = net.createServer((socket: any) => {
         } else if (path === '/user-agent') {
             const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-        } else if (path === `/files/`) {
+        } else if (path === `/files`) {
             const directory = process.argv[3];
             const fileName = query;
 
